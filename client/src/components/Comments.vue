1dc6934224c580d827d23ea43252d7d8de4a29b6
Abidur Rahman Dipta <rahmanabid083@gmail.com>
2022-02-12 13:03:24 -0500

    changed comment style

--- client/src/components/Comments.vue
+++ client/src/components/Comments.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
     <div :key="element.id" v-for="element in comments">
-      <div v-if="parseInt($route.params.id) === element.product_id">
-        <h3>{{ element.username }}</h3>
-        <p>{{ element.content }}</p>
-        <button @click="deleteComment(element.id)">Delete</button>
+      <div
+        v-if="parseInt($route.params.id) === element.product_id"
+        class="form"
+      >
+        <ul>
+          <li>
+            <h3>{{ element.username }}</h3>
+          </li>
+          <li>
+            <p>{{ element.content }}</p>
+          </li>
+          <li>
+            <button @click="deleteComment(element.id)">Delete</button>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -32,3 +43,76 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+/* Style design was inspired from sanwebe.com */
+.form {
+  max-width: 750px;
+  background: #fafafa;
+  padding: 30px;
+  margin: 50px auto;
+  box-shadow: 1px 1px 25px rgba(0, 0, 0, 0.35);
+  border-radius: 10px;
+  border: 6px solid #305a72;
+}
+.form ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+.form ul li {
+  display: block;
+  margin-bottom: 10px;
+  min-height: 35px;
+}
+.form ul li .field-style {
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  padding: 8px;
+  outline: none;
+  border: 1px solid #b0cfe0;
+}
+.form ul li .field-style:focus {
+  box-shadow: 0 0 5px #b0cfe0;
+  border: 1px solid #b0cfe0;
+}
+.form ul li .field-split {
+  width: 49%;
+}
+.form ul li .field-full {
+  width: 100%;
+}
+.form ul li input.align-left {
+  float: left;
+}
+.form ul li input.align-right {
+  float: right;
+}
+
+.form ul li button,
+.form ul li button[type='submit'] {
+  -moz-box-shadow: inset 0px 1px 0px 0px #3985b1;
+  -webkit-box-shadow: inset 0px 1px 0px 0px #3985b1;
+  box-shadow: inset 0px 1px 0px 0px #4b0505;
+  background-color: #c22a2a;
+  border: 1px solid #5e1010;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  padding: 8px 18px;
+  text-decoration: none;
+  font: 12px Arial, Helvetica, sans-serif;
+}
+.form ul li button:hover,
+.form ul li button[type='submit']:hover {
+  background: linear-gradient(to bottom, #761d1d 5%, #bb3d45 100%);
+  background-color: #ac3e3e;
+}
+
+@media (max-width: 450px) {
+  .form {
+    max-width: 250px;
+  }
+}
+</style>
